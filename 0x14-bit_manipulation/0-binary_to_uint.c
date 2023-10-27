fdddd3c7e4ccdad41cb5f71f3369de52d9ae34e0
cessdaniels <musyokicicilia3@gmail.com>
2023-10-27 03:50:59 -0700

    binary manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0 ;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
-		return 0;
+	if (!b)
+		return (0);
 
+	ui = 0;
 
-	while (*b)
+	for (len = 0; b[len] != '0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*b != '0' && *b != '1')
-			return 0;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-		result = (result * 2) + (*b - '0');
-		b++;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
 
