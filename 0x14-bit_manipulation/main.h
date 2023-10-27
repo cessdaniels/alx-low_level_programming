69ccbe50a550bdee04cce0aaf25b038da0b8474f
cessdaniels <musyokicicilia3@gmail.com>
2023-10-27 02:23:58 -0700

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  *
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-Bit manipulation directory.
  */
 
 int _putchar(char c);
