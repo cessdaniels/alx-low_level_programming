151b323d422442a98637167abc207dada455f899
cessdaniels <musyokicicilia3@gmail.com>
2023-10-30 03:47:20 -0700

    fies

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,21 @@
  * or if it fails.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char  *text_content)
 {
 	int fd;
 	int nletters;
 	int rwr;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
 		rwr = write(fd, text_content, nletters);
 
